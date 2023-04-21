ad6f032fe7830e6d850256529d6641d39f1b6ea6
davidecaputo <daca9621@gmail.com>
2023-04-21 17:36:54 +0200

    Fix errors

--- src/components/PrintCards.vue
+++ src/components/PrintCards.vue
@@ -1,11 +1,11 @@
 <template>
     <h2 class="pb-4">Film:</h2>
     <div class="row">
-        <div v-for="(film, index) of store.film" :key="film.id" class="col-12 col-sm-6 col-md-4 col-lg-3">
-            <div class="card mb-4 w-100 h-100">
+        <div v-for="(film, index) of store.film" :key="film.id" class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+            <div class="card dc-card w-100 h-100">
                 <img :src="store.imageUrl + film.poster_path" alt="" v-if="film.poster_path">
-                <img src="https://www.cuneo24.it/photogallery_new/images/2018/07/cinema-film-generica-4657.660x368.jpg" alt="" class="image-not-found" v-else>
-                <div class="card-body">
+                <img src="https://www.cuneo24.it/photogallery_new/images/2018/07/cinema-film-generica-4657.660x368.jpg" alt="" v-else>
+                <div class="card-body dc-card-body">
                     <h2>{{ film.title }}</h2>
                     <p>{{ film.original_title }}</p>
                     <p class="fi" :class="(film.original_language === 'en')?'fi-us' : 'fi-' + film.original_language"></p>
@@ -16,11 +16,11 @@
     </div>
     <h2 class="pb-4">Serie TV:</h2>
     <div class="row">
-        <div v-for="(serie, index) of store.serieTv" :key="serie.id" class="col-12 col-sm-6 col-md-4 col-lg-3">
-            <div class="card mb-4 w-100 h-100">
+        <div v-for="(serie, index) of store.serieTv" :key="serie.id" class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+            <div class="card dc-card w-100 h-100">
                 <img :src="store.imageUrl + serie.poster_path" alt="" v-if="serie.poster_path">
-                <img src="https://www.cuneo24.it/photogallery_new/images/2018/07/cinema-film-generica-4657.660x368.jpg" alt="" class="image-not-found" v-else>
-                <div class="card-body">
+                <img src="https://www.cuneo24.it/photogallery_new/images/2018/07/cinema-film-generica-4657.660x368.jpg" alt="" v-else>
+                <div class="card-body dc-card-body">
                     <h4>{{ serie.name }}</h4>
                     <p>{{ serie.original_title }}</p>
                     <p class="fi" :class="(serie.original_language === 'en')?'fi-us' : 'fi-' + serie.original_language"></p>
@@ -45,7 +45,12 @@ import "/node_modules/flag-icons/css/flag-icons.min.css";
 </script>
 
 <style lang="scss" scoped>
-    .card{
+    .dc-card{
+        & img{
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+        }
         &:hover{
             transform: rotateY(360deg);
             box-shadow: 4px 4px 20px 10px;
@@ -54,7 +59,7 @@ import "/node_modules/flag-icons/css/flag-icons.min.css";
         &:not( :hover ){
             transition: 1s;
         }
-        & .card-body{
+        & .dc-card-body{
             position: absolute;
             top: 0;
             bottom: 0;
@@ -63,15 +68,10 @@ import "/node_modules/flag-icons/css/flag-icons.min.css";
             opacity: 0;
             color: white;
         }
-        & .card-body:hover{
+        & .dc-card-body:hover{
             opacity: 1;
             background-color: #181818;
             transition: 1s;
-        }
-        & img.image-not-found{
-            width: 100%;
-            height: 100%;
-            object-fit: cover;
         }
     }
 </style>
